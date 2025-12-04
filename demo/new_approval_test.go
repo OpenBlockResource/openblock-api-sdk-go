d0e66c02021bf3e87fc04d0b3c58f99c67a1ace2
huajun <chen1911402@gmail.com>
2025-12-04 15:41:17 +0800

    add sol sign

--- demo/new_approval_test.go
+++ demo/new_approval_test.go
@@ -53,3 +53,31 @@ func TestNewBtcApproval(t *testing.T) {
 	})
 	fmt.Println(resp, err)
 }
+
+func TestNewSolTransactionSignatureApproval(t *testing.T) {
+	client := apisdk.NewClient("361bdf3a1e0640979a3e2240c3361609", "U3IXmFgR848q5XA1vQVgdvW1Z69UvQXD", 10*time.Second)
+	// client.CompanyWallet.Host = "hkdev8back.openblock.vip"
+	resp, err := client.CompanyWallet.NewApproval(&apisdk.ParamNewApproval{
+		Action: "TRANSACTION_SIGNATURE",
+		TXInfo: apisdk.TXInfo{
+			Chain:  "Solana",
+			Type:   "dapp",
+			Method: "solana_signMessage",
+			Msg: &apisdk.Msg{
+				SignMsg:     "7b226e6f6e6365223a223631313061393939636634323962303632343939383632633538363934613064222c2274696d657374616d70223a313736343735303235373335337d",
+				Message:     "{\"nonce\":\"6110a999cf429b062499862c58694a0d\",\"timestamp\":1764750257353}",
+				OriginalMsg: "7b226e6f6e6365223a223631313061393939636634323962303632343939383632633538363934613064222c2274696d657374616d70223a313736343735303235373335337d",
+			},
+			DappInfo: &apisdk.DappInfo{
+				Origin:   "https://app.unitas.so",
+				Href:     "https://app.unitas.so/earn",
+				PortName: "https://app.unitas.so1764750194493",
+				Icon:     "",
+				Provider: "solana",
+				DappName: "",
+			},
+			OriginAddress: "HM7LoUtJgRfChJv1m9LJH2b4vJ6W6Qn7on3sUptCQYNT",
+		},
+	})
+	fmt.Println(resp, err)
+}
