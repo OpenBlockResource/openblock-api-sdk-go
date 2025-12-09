a5b95c05addbc26e1e7e8fb80149ae632ee99593
huajun <chen1911402@gmail.com>
2025-12-09 11:50:05 +0800

    add Authorization

--- demo/get_approvals_test.go
+++ demo/get_approvals_test.go
@@ -19,7 +19,8 @@ func TestGetApprovals(t *testing.T) {
 }
 
 func TestGetApprovalsV2(t *testing.T) {
-	client := apisdk.NewClient("APIKey", "APISecret", 10*time.Second)
+	client := apisdk.NewClient("361bdf3a1e0640979a3e2240c3361609", "U3IXmFgR848q5XA1vQVgdvW1Z69UvQXD", 10*time.Second)
+	client.CompanyWallet.Host = "hkdev8back.openblock.vip"
 	resp, err := client.CompanyWallet.GetApprovalsV2(&apisdk.ParamGetApprovalsV2{
 		Page:     1,
 		Limit:    20,
